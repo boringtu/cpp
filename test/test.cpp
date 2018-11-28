0b585d726d0f6d7c3d7bb04002d62b74355f5b39
BoringTu <mail@boringtu.com>
2018-11-28 14:22:53 +0800

    + init

---
+++ test/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+// void nextCoord(int *coord[]) {
+// 	int x = coord[0];
+// 	int y = coord[1];
+	
+// }
+int main() {
+	// int n = 1;	// 初始化的数字（从1开始）
+
+	// int len1 = 4;	// 一维数组长度
+	// int len2 = 4;	// 二维数组长度
+
+	// int i1 = 0;	// 一维数组下标
+	// int i2 = 0;	// 二维数组下标
+
+	// int re[len1][len2];
+
+	// while(1) {
+	// 	x
+	// }
+
+
+	// cout << re[i1][i2];
+	cout << " | ";
+	// cout << re[i1][i2];
+	cout << endl;
+	return 0;
+
+
+
+
+
+
+}
